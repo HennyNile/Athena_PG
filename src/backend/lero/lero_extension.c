4b29049063f3c41c4c4108d547a15cf9d015b072
HennyNile <11811410@mail.sustech.edu.cn>
2024-06-09 10:50:20 +0000

    Fix: Wrong number of tables and possible memory problem.

--- src/backend/lero/lero_extension.c
+++ src/backend/lero/lero_extension.c
@@ -1,5 +1,6 @@
 #include "lero/lero_extension.h"
 #include "optimizer/planner.h"
+#include "utils/lsyscache.h"
 
 #define CARD_MAX_NUM 25000
 
@@ -24,37 +25,93 @@ add_join_input_tables(PlannerInfo *root, Path *path, int* num)
 	SortPath* sort_path;
 	AggPath* agg_path;
 	MemoizePath* memoize_path;
-	
+	GatherPath* gather_path;
+	GatherMergePath* gather_merge_path;
+	Index table_relid;
+	char* table_name;
+
 	switch (path->pathtype)
 	{
 		case T_SeqScan:
+			table_relid = path->parent->relid;
+			table_name = get_rel_name(root->simple_rte_array[table_relid]->relid);
+			// elog(WARNING, "Seq Scan %s", table_name);
+			*num += 1;
+            break;
 		case T_IndexScan:
+			table_relid = path->parent->relid;
+			table_name = get_rel_name(root->simple_rte_array[table_relid]->relid);
+			// elog(WARNING, "Index Scan %s", table_name);
+			*num += 1;
+            break;
 		case T_IndexOnlyScan:
-		case T_BitmapHeapScan:;
-			num += 1;
+			table_relid = path->parent->relid;
+			table_name = get_rel_name(root->simple_rte_array[table_relid]->relid);
+			// elog(WARNING, "Index Only Scan %s", table_name);
+			*num += 1;
+            break;
+		case T_BitmapHeapScan:
+			table_relid = path->parent->relid;
+			table_name = get_rel_name(root->simple_rte_array[table_relid]->relid);
+			// elog(WARNING, "Bitmap Heap Scan %s", table_name);
+			*num += 1;
             break;
 		case T_HashJoin:
-		case T_MergeJoin:
-		case T_NestLoop:;
 			join_path = (JoinPath *) path;
+			// elog(WARNING, "Hash Join (");
 			add_join_input_tables(root, join_path->innerjoinpath, num);
 			add_join_input_tables(root, join_path->outerjoinpath, num);
+			// elog(WARNING, ")");
+			break;
+		case T_MergeJoin:
+			join_path = (JoinPath *) path;
+			// elog(WARNING, "Merge Join (");
+			add_join_input_tables(root, join_path->innerjoinpath, num);
+			add_join_input_tables(root, join_path->outerjoinpath, num);
+			// elog(WARNING, ")");
+			break;
+		case T_NestLoop:
+			join_path = (JoinPath *) path;
+			// elog(WARNING, "NestLoop Join (");
+			add_join_input_tables(root, join_path->innerjoinpath, num);
+			add_join_input_tables(root, join_path->outerjoinpath, num);
+			// elog(WARNING, ")");
 			break;
 		case T_Material:;
+			// elog(WARNING, "Material (");
 			material_path = (MaterialPath *) path;
 			add_join_input_tables(root, material_path->subpath, num);
+			// elog(WARNING, ")");
 			break;
 		case T_Sort:;
+			// elog(WARNING, "Sort (");
 			sort_path = (SortPath *) path;
 			add_join_input_tables(root, sort_path->subpath, num);
+			// elog(WARNING, ")");
 			break;
 		case T_Agg:;
+			// elog(WARNING, "Agg (");
 			agg_path = (AggPath *) path;
 			add_join_input_tables(root, agg_path->subpath, num);
+			// elog(WARNING, ")");
 			break;
 		case T_Memoize:;
+			// elog(WARNING, "Memoize (");
 			memoize_path = (MemoizePath *) path;
 			add_join_input_tables(root, memoize_path->subpath, num);
+			// elog(WARNING, ")");
+			break;
+		case T_Gather:
+			// elog(WARNING, "Gather (");
+			gather_path = (GatherPath *) path;
+			add_join_input_tables(root, gather_path->subpath, num);
+			// elog(WARNING, ")");
+			break;
+		case T_GatherMerge:
+			// elog(WARNING, "Gather Merge (");
+			gather_merge_path = (GatherMergePath *) path;
+			add_join_input_tables(root, gather_merge_path->subpath, num);
+			// elog(WARNING, ")");
 			break;
 		case T_SampleScan:
 		case T_TidScan:
@@ -72,7 +129,6 @@ add_join_input_tables(PlannerInfo *root, Path *path, int* num)
 		case T_Result:
 		case T_ProjectSet:
 		case T_Unique:
-		case T_Gather:
 		case T_IncrementalSort:
 		case T_Group:
 		case T_WindowAgg:
@@ -80,7 +136,8 @@ add_join_input_tables(PlannerInfo *root, Path *path, int* num)
 		case T_LockRows:
 		case T_ModifyTable:
 		case T_Limit:
-		case T_GatherMerge:
+			elog(ERROR, "unrecognized node type: %d",
+				 (int) path->pathtype);
 			break;
 		default:
 			elog(ERROR, "unrecognized node type: %d",
@@ -99,23 +156,28 @@ void lero_pgsysml_set_joinrel_size_estimates(PlannerInfo *root, RelOptInfo *rel,
 	int num_tables = 0;
 	if (record_original_card_phase)
 	{
-		rows = rel->rows;
-		original_card_list[join_card_num] = rows;
+		if (join_card_num < CARD_MAX_NUM) {
+			rows = rel->rows;
+			original_card_list[join_card_num] = rows;
 
-		num_tables = 0;
-		add_join_input_tables(root, outer_rel->cheapest_total_path, &num_tables);
-		add_join_input_tables(root, inner_rel->cheapest_total_path, &num_tables);
-		join_input_table_nums[join_card_num] = num_tables;
-		join_card_num += 1;
+			num_tables = 0;
+			add_join_input_tables(root, outer_rel->cheapest_total_path, &num_tables);
+			add_join_input_tables(root, inner_rel->cheapest_total_path, &num_tables);
+			join_input_table_nums[join_card_num] = num_tables;
+
+			// elog(WARNING, "estimate card %d: rows %f, num_table %d", join_card_num, rows, num_tables);
+			join_card_num += 1;
+		}
 	}
 	else
 	{
-		if (cur_card_idx <= join_card_num) {
+		if (cur_card_idx < join_card_num) {
+			// elog(WARNING, "set card %d: %f", cur_card_idx, lero_card_list[cur_card_idx]);
 			rows = lero_card_list[cur_card_idx];
+			cur_card_idx += 1;
 		} else {
 			rows = rel->rows;
 		}
-		cur_card_idx += 1;
 	}
 	rel->rows = rows;
 }
@@ -134,6 +196,7 @@ PlannedStmt *lero_pgsysml_hook_planner(Query *parse, const char *queryString,
 	record_original_card_phase = false;
 	for (int i = 0; i < join_card_num; i++) {
 		if (join_input_table_nums[i] == lero_subquery_table_num) {
+			// elog(WARNING, "change card %d: from %f to %f", i, original_card_list[i], original_card_list[i] * lero_swing_factor);
 			lero_card_list[i] = original_card_list[i] * lero_swing_factor;
 		} else {
 			lero_card_list[i] = original_card_list[i];
